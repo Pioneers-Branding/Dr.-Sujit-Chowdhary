6adb742cb799301a94af822e254ce9a72a1704be
anshrealestate <anshrealestate@gmail.com>
2026-08-21 18:03:18 +0530

    Add Google Search Console verification meta tag

--- includes/head.php
+++ includes/head.php
@@ -17,6 +17,7 @@ $css_version = file_exists(__DIR__ . '/../css/index.css') ? filemtime(__DIR__ . 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="google-site-verification" content="VddElzDxJ0cU9h7VtHki04UeDrujkgGkfRoH7KqkmC8">
     <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
     <meta http-equiv="Pragma" content="no-cache">
     <meta http-equiv="Expires" content="0">
